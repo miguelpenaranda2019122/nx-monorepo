3f9a4daaa2498b68af900c514800a3cdc41c1d1a
miguelpenaranda2019122 <2019122@student.uma.pt>
2024-03-25 13:45:21 +0000

    react-native-config Android

--- apps/todov3/android/app/src/main/java/com/todov3/MainApplication.kt
+++ apps/todov3/android/app/src/main/java/com/todov3/MainApplication.kt
@@ -11,16 +11,18 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.facebook.react.shell.MainReactPackage;
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return PackageList(this).packages
-        }
+        val packages = mutableListOf<ReactPackage>()
+        // packages.add(RNCConfigPackage())
+        packages.addAll(PackageList(this).packages)
+        return packages
+    }
 
         override fun getJSMainModuleName(): String = "src/main"
 
